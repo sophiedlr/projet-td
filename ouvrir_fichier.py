31c927c48bb66e03e4069af139aad1a7faede809
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-10-20 10:11:27 +0200

    Create ouvrir_fichier.py

---
+++ ouvrir_fichier.py
@@ -0,0 +1,21 @@
+def ouvrir_fichier() :
+
+    file = open("D:\Documents\EIVP\IA2\Info\Projet\EIVP_KM.csv")
+    content = file.read()
+    file.close()
+
+    content = content.split("\n")
+    tableau = []
+    for ligne in content :
+        ligne = ligne.split(";")
+        tableau.append(ligne)
+
+    titres = tableau[0]
+    tableau = tableau[1:len(tableau)-1] #on enlève la première ligne (les titres) et la dernière ligne (qui est vide)
+
+    for i in range(0, len(tableau)-1) :
+        for j in range(0, 6) :
+            a = tableau[i][j]
+            tableau[i][j] = float(a)
+
+    return (titres, tableau)
