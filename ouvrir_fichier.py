134b2406812523963c7989586d97fc1049aaed98
CarolineNz <caroline.nouzieres@eivp-paris.fr>
2020-10-23 15:14:17 +0200

    Update ouvrir_fichier.py

--- ouvrir_fichier.py
+++ ouvrir_fichier.py
@@ -2,7 +2,7 @@ def ouvrir_fichier() :
     '''Ouvre le fichier de données et le convertit en variables exploitables
     renvoie : (titres[liste de chaines], tableau[liste de listes de données]) '''
 
-    file = open("./EIVP_KM.csv")
+    file = open("D:\Documents\GitHub\projet-td\EIVP_KM.csv")
     content = file.read()
     file.close()
 
